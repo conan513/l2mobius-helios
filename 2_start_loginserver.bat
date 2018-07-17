c9a58426afd9290a8349aa86b48e3d36e1da9171
Conan <conan_513@hotmail.com>
2018-07-17 04:00:16 +0200

    Fix path issues

--- 2_start_loginserver.bat
+++ 2_start_loginserver.bat
@@ -5,5 +5,5 @@ set JAVA_HOME=%mainfolder%\Java
 cd "%mainfolder%\login"
 echo Starting loginserver...
 echo.
-start %JAVA_HOME%\bin\javaw.exe -jar ../libs/LoginServer.jar
+start "%JAVA_HOME%"\bin\javaw.exe ../libs/LoginServer.jar
 exit
--- 3_start_gameserver.bat
+++ 3_start_gameserver.bat
@@ -3,7 +3,7 @@ set mainfolder=%CD%
 set JAVA_HOME=%mainfolder%\Java
 
 cd "%mainfolder%\game"
-echo Starting loginserver...
+echo Starting gameserver...
 echo.
-start %JAVA_HOME%\bin\javaw.exe -jar -server -Dfile.encoding=UTF-8 -Djava.util.logging.manager=com.l2jmobius.log.L2LogManager -XX:+AggressiveOpts -Xnoclassgc -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -XX:+UseParNewGC -XX:SurvivorRatio=8 -Xmx4g -Xms2g -Xmn1g ../libs/GameServer.jar
+start "%JAVA_HOME%"\bin\javaw.exe -server -Dfile.encoding=UTF-8 -Djava.util.logging.manager=com.l2jmobius.log.L2LogManager -XX:+AggressiveOpts -Xnoclassgc -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -XX:+UseParNewGC -XX:SurvivorRatio=8 -Xmx4g -Xms2g -Xmn1g ../libs/GameServer.jar
 exit
